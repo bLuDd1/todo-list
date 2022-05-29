61c24bc09c0f5bfe40e3ed4ac7d0cb06f4113a7c
bLuDd1 <dtavluj@gmail.com>
2022-05-29 17:23:02 +0300

    Added class that adds tasks to array.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+'use strict';
 
+const addTask = document.getElementById('add-task');
+const descriptionTask = document.getElementById('task');
+const todosContainer = document.getElementsByClassName('todos-container');
+
+const tasks = [];
+
+// Creating new tasks
+class Task {
+  constructor(description) {
+    this.description = description;
+    this.completed = false;
+  }
+}
+
+// Adding new tasks to array
+addTask.addEventListener('click', () => {
+  tasks.push(new Task(descriptionTask.value));
+});
