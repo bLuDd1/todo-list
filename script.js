08b5ddb241d62f57272fb50f6b7943c317b0933d
bLuDd1 <dtavluj@gmail.com>
2022-06-02 23:26:00 +0300

    Added functions to show tasks on screen.

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const addTask = document.getElementById('add-task');
 const descriptionTask = document.getElementById('task');
 const todosContainer = document.getElementsByClassName('todos-container');
 
-const tasks = [];
+let tasks;
+if (!localStorage.tasks) {
+  tasks = [];
+} else {
+  tasks = JSON.parse(localStorage.getItem('tasks'));
+}
 
 // Creating new tasks
 class Task {
@@ -18,10 +23,31 @@ const updateStorage = () => {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
+// Shows tasks at page screen
+const createTemplate = task =>
+  `<div class="tasks-items">
+                <div class="description">${task.description}</div>
+                <div class="buttons">
+                    <input class="btn-complete" type="checkbox">
+                    <button class="btn-delete">Delete</button>
+                </div>
+            </div>`
+  ;
+
+const fillTodosCont = () => {
+  todosContainer.innerHTML = '';
+  if (tasks.length > 0) {
+    for (const key of tasks) {
+      todosContainer.innerHTML = createTemplate(key);
+    }
+  }
+};
+
 // Adding new tasks to array
 const addNewTask = () => {
   tasks.push(new Task(descriptionTask.value));
   updateStorage();
+  fillTodosCont();
 };
 
 addTask.onclick = addNewTask;
